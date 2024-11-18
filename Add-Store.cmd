710ae477ce3ead2ffe9ed5add20ab91e3d9af1c2
ergosteur <ergosteur@gmail.com>
2024-11-18 17:49:33 -0500

    Add WindowsTerminal, update DesktopAppInstaller to download from MS, Add Xaml2.8 dependency

--- Add-Store.cmd
+++ Add-Store.cmd
@@ -8,6 +8,18 @@ cd /d "%~dp0"
 if not exist "*WindowsStore*.msixbundle" goto :nofiles
 if not exist "*WindowsStore*.xml" goto :nofiles
 
+:: Check if DesktopAppInstaller MSIX bundle exists and download if missing
+if not exist "*DesktopAppInstaller*.msixbundle" (
+    echo Desktop App Installer msixbundle not found. Downloading now...
+    powershell -Command "$response = Invoke-WebRequest -Uri 'https://aka.ms/getwinget'; if ($response.Headers['Content-Disposition'] -match 'filename=''(.+?)''') {$filename = $matches[1]} else {$filename = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'}; [System.IO.File]::WriteAllBytes($filename, $response.Content)"
+    if %errorlevel% neq 0 (
+        echo Error downloading Desktop App Installer. Please check your internet connection and try again.
+        exit /b 1
+    )
+    echo Download complete.
+)
+
+
 for /f %%i in ('dir /b *WindowsStore*.msixbundle 2^>nul') do set "Store=%%i"
 for /f %%i in ('dir /b *NET.Native.Framework*.appx 2^>nul ^| find /i "x64"') do set "Framework6X64=%%i"
 for /f %%i in ('dir /b *NET.Native.Framework*.appx 2^>nul ^| find /i "x86"') do set "Framework6X86=%%i"
@@ -18,9 +30,12 @@ for /f %%i in ('dir /b *NET.Native.Runtime*.appx 2^>nul ^| find /i "arm64"') do 
 for /f %%i in ('dir /b *VCLibs*140.00_14*.appx 2^>nul ^| find /i "x64"') do set "VCLibsX64=%%i"
 for /f %%i in ('dir /b *VCLibs*140.00_14*.appx 2^>nul ^| find /i "x86"') do set "VCLibsX86=%%i"
 for /f %%i in ('dir /b *VCLibs*140.00_14*.appx 2^>nul ^| find /i "arm64"') do set "VCLibsArm64=%%i"
-for /f %%i in ('dir /b *Microsoft.UI.Xaml*.appx 2^>nul ^| find /i "x64"') do set "XamlX64=%%i"
-for /f %%i in ('dir /b *Microsoft.UI.Xaml*.appx 2^>nul ^| find /i "x86"') do set "XamlX86=%%i"
-for /f %%i in ('dir /b *Microsoft.UI.Xaml*.appx 2^>nul ^| find /i "arm64"') do set "XamlArm64=%%i"
+for /f %%i in ('dir /b *Microsoft.UI.Xaml.2.8*.appx 2^>nul ^| find /i "x64"') do set "Xaml28X64=%%i"
+for /f %%i in ('dir /b *Microsoft.UI.Xaml.2.8*.appx 2^>nul ^| find /i "x86"') do set "Xaml28X86=%%i"
+for /f %%i in ('dir /b *Microsoft.UI.Xaml.2.8*.appx 2^>nul ^| find /i "arm64"') do set "Xaml28Arm64=%%i"
+for /f %%i in ('dir /b *Microsoft.UI.Xaml.2.4*.appx 2^>nul ^| find /i "x64"') do set "Xaml24X64=%%i"
+for /f %%i in ('dir /b *Microsoft.UI.Xaml.2.4*.appx 2^>nul ^| find /i "x86"') do set "Xaml24X86=%%i"
+for /f %%i in ('dir /b *Microsoft.UI.Xaml.2.4*.appx 2^>nul ^| find /i "arm64"') do set "Xaml24Arm64=%%i"
 for /f %%i in ('dir /b *VCLibs*140.00.UWPDesktop*.appx 2^>nul ^| find /i "x64"') do set "VCLibsUWPX64=%%i"
 for /f %%i in ('dir /b *VCLibs*140.00.UWPDesktop*.appx 2^>nul ^| find /i "x86"') do set "VCLibsUWPX86=%%i"
 for /f %%i in ('dir /b *VCLibs*140.00.UWPDesktop*.appx 2^>nul ^| find /i "arm64"') do set "VCLibsUWPArm64=%%i"
@@ -28,29 +43,33 @@ for /f %%i in ('dir /b *VCLibs*140.00.UWPDesktop*.appx 2^>nul ^| find /i "arm64"
 if exist "*StorePurchaseApp*.appxbundle" if exist "*StorePurchaseApp*.xml" (
 for /f %%i in ('dir /b *StorePurchaseApp*.appxbundle 2^>nul') do set "PurchaseApp=%%i"
 )
-if exist "*DesktopAppInstaller*.appxbundle" if exist "*DesktopAppInstaller*.xml" (
-for /f %%i in ('dir /b *DesktopAppInstaller*.appxbundle 2^>nul') do set "AppInstaller=%%i"
+if exist "*DesktopAppInstaller*.msixbundle" if exist "*DesktopAppInstaller*.xml" (
+for /f %%i in ('dir /b *DesktopAppInstaller*.msixbundle 2^>nul') do set "AppInstaller=%%i"
 )
 if exist "*XboxIdentityProvider*.appxbundle" if exist "*XboxIdentityProvider*.xml" (
 for /f %%i in ('dir /b *XboxIdentityProvider*.appxbundle 2^>nul') do set "XboxIdentity=%%i"
 )
 
+if exist "*Microsoft.WindowsTerminal*.msixbundle" if exist "*Microsoft.WindowsTerminal*.xml" (
+for /f %%i in ('dir /b *Microsoft.WindowsTerminal*.msixbundle 2^>nul') do set "WindowsTerminal=%%i"
+)
+
 if /i %arch%==x64 (
-    set "DepStore=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%,%XamlX64%,%XamlX86%,%VCLibsUWPX64%,%VCLibsUWPX86%"
-    set "DepPurchase=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%,%XamlX64%,%XamlX86%,%VCLibsUWPX64%,%VCLibsUWPX86%"
+    set "DepStore=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%,%Xaml24X64%,%Xaml24X86%,%Xaml28X64%,%Xaml28X86%,%VCLibsUWPX64%,%VCLibsUWPX86%"
+    set "DepPurchase=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%,%Xaml28X64%,%Xaml28X86%,%VCLibsUWPX64%,%VCLibsUWPX86%"
     set "DepXbox=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%"
-    set "DepInstaller=%VCLibsX64%,%VCLibsX86%,%XamlX64%,%XamlX86%,%VCLibsUWPX64%,%VCLibsUWPX86%"
+    set "DepInstaller=%VCLibsX64%,%VCLibsX86%,%Xaml28X64%,%Xaml28X86%,%VCLibsUWPX64%,%VCLibsUWPX86%"
 ) else (
     if /i %arch%==arm64 (
-        set "DepStore=%VCLibsArm64%,%VCLibsX64%,%VCLibsX86%,%Framework6Arm64%,%Framework6X64%,%Framework6X86%,%Runtime6Arm64%,%Runtime6X64%,%Runtime6X86%,%XamlArm64%,%XamlX64%,%XamlX86%,%VCLibsUWPArm64%,%VCLibsUWPX64%,%VCLibsUWPX86%"
-        set "DepPurchase=%VCLibsArm64%,%VCLibsX64%,%VCLibsX86%,%Framework6Arm64%,%Framework6X64%,%Framework6X86%,%Runtime6Arm64%,%Runtime6X64%,%Runtime6X86%,%XamlArm64%,%XamlX64%,%XamlX86%,%VCLibsUWPArm64%,%VCLibsUWPX64%,%VCLibsUWPX86%"
+        set "DepStore=%VCLibsArm64%,%VCLibsX64%,%VCLibsX86%,%Framework6Arm64%,%Framework6X64%,%Framework6X86%,%Runtime6Arm64%,%Runtime6X64%,%Runtime6X86%,%Xaml24arm64%,%Xaml24X64%,%Xaml24X86%,%Xaml28arm64%,%Xaml28X64%,%Xaml28X86%,%VCLibsUWPArm64%,%VCLibsUWPX64%,%VCLibsUWPX86%"
+        set "DepPurchase=%VCLibsArm64%,%VCLibsX64%,%VCLibsX86%,%Framework6Arm64%,%Framework6X64%,%Framework6X86%,%Runtime6Arm64%,%Runtime6X64%,%Runtime6X86%,%Xaml28arm64%,%Xaml28X64%,%Xaml28X86%,%VCLibsUWPArm64%,%VCLibsUWPX64%,%VCLibsUWPX86%"
         set "DepXbox=%VCLibsArm64%,%VCLibsX64%,%VCLibsX86%,%Framework6Arm64%,%Framework6X64%,%Framework6X86%,%Runtime6Arm64%,%Runtime6X64%,%Runtime6X86%"
-        set "DepInstaller=%VCLibsArm64%,%VCLibsX64%,%VCLibsX86%,%XamlArm64%,%XamlX64%,%XamlX86%,%VCLibsUWPArm64%,%VCLibsUWPX64%,%VCLibsUWPX86%"
+        set "DepInstaller=%VCLibsArm64%,%VCLibsX64%,%VCLibsX86%,%Xaml28arm64%,%Xaml28X64%,%Xaml28X86%,%VCLibsUWPArm64%,%VCLibsUWPX64%,%VCLibsUWPX86%"
     ) else (
-        set "DepStore=%VCLibsX86%,%Framework6X86%,%Runtime6X86%,%XamlX86%,%VCLibsUWPX86%"
-        set "DepPurchase=%VCLibsX86%,%Framework6X86%,%Runtime6X86%,%XamlX86%,%VCLibsUWPX86%"
+        set "DepStore=%VCLibsX86%,%Framework6X86%,%Runtime6X86%,%Xaml24X86%,%Xaml28X86%,%VCLibsUWPX86%"
+        set "DepPurchase=%VCLibsX86%,%Framework6X86%,%Runtime6X86%,%Xaml28X86%,%VCLibsUWPX86%"
         set "DepXbox=%VCLibsX86%,%Framework6X86%,%Runtime6X86%"
-        set "DepInstaller=%VCLibsX86%,%XamlX86%,%VCLibsUWPX86%"
+        set "DepInstaller=%VCLibsX86%,%Xaml28X86%,%VCLibsUWPX86%"
     )
 )
 
@@ -98,6 +117,17 @@ echo.
 1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %XboxIdentity% -DependencyPackagePath %DepXbox% -LicensePath Microsoft.XboxIdentityProvider_8wekyb3d8bbwe.xml
 %PScommand% Add-AppxPackage -Path %XboxIdentity%
 )
+if defined WindowsTerminal (
+echo.
+echo ============================================================
+echo Adding WindowsTerminal
+echo ============================================================
+echo.
+1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %WindowsTerminal% -DependencyPackagePath %DepInstaller% -LicensePath Microsoft.WindowsTerminal_8wekyb3d8bbwe.xml
+%PScommand% Add-AppxPackage -Path %WindowsTerminal%
+)
+
+
 goto :fin
 
 :uac
